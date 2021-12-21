9d4535fba42655d5b8eb54420c2bf92c3a3374f6
Ahmed Musawir <ahmed.musawir@gmail.com>
2021-12-21 08:49:56 +0800

    WPAPI with READ,DELETE and EDIT operation to the onpage React app was added - 21 Dec 2021

---
+++ template-react-wp-app-1.php
@@ -0,0 +1,24 @@
+<?php
+ /**
+  * The template for displaying all pages
+  * Template Name: React WP App 1
+  * @package cyberize-app-dev
+  */
+
+ get_header();
+?>
+
+<header id="header-test" class="site-header container text-center">
+
+  <h1 class="pt-3">React WP App One</h1>
+  <h4>(from wp-oop-php theme)</h4>
+  <hr>
+
+</header><!-- #masthead -->
+
+<div id="react-wp-app-1"></div>
+
+</main><!-- #main -->
+
+<?php
+get_footer();
